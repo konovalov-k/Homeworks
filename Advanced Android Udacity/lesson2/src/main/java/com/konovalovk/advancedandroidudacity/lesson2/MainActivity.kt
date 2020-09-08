2437621607c6bbd40e4d42eb5dd14c5a5b177b9d
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-08 12:29:41 +0300

    Lesson 2 inited

---
+++ Advanced Android Udacity/lesson2/src/main/java/com/konovalovk/advancedandroidudacity/lesson2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.konovalovk.advancedandroidudacity.lesson2
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.main_activity) {}
---
+++ Advanced Android Udacity/lesson2/src/main/java/com/konovalovk/advancedandroidudacity/lesson2/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.konovalovk.advancedandroidudacity.lesson2
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
