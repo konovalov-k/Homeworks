d114bc4567f75a9c5d14d25c14c66c117781b3b0
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-09 14:17:13 +0300

    Exercise 10. setup Paint - done

--- Advanced Android Udacity/lesson4/src/main/java/com/konovalovk/advancedandroidudacity/lesson4/MyCanvasView.kt
+++ Advanced Android Udacity/lesson4/src/main/java/com/konovalovk/advancedandroidudacity/lesson4/MyCanvasView.kt
@@ -3,9 +3,11 @@ package com.konovalovk.advancedandroidudacity.lesson4
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Paint
 import android.view.View
 import androidx.core.content.res.ResourcesCompat
 
+private const val STROKE_WIDTH = 12f // has to be float
 //Todo: 1.1 Create MyCanvasView
 class MyCanvasView(context: Context) : View(context) {
     //Todo: 1.3 Init some Canvas & Bitmap
@@ -13,6 +15,25 @@ class MyCanvasView(context: Context) : View(context) {
     private var extraCanvas: Canvas = Canvas(extraBitmap)
 
     private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+    private val drawColor = ResourcesCompat.getColor(resources, R.color.colorPaint, null)
+
+    //Todo: 1.6 Set up the paint with which to draw.
+    private val paint = Paint().apply {
+        color = drawColor
+        // Smooths out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        // Dithering affects how colors with higher-precision than the device are down-sampled.
+        isDither = true
+        //Paint.style specifies if the primitive being drawn is filled, stroked, or both.
+        style = Paint.Style.STROKE // default: FILL
+        //Stroke join of paints.join specifies how lines and curve segments join a stroked path.
+        strokeJoin = Paint.Join.ROUND // default: MITER
+        //StrokeCap sets the shape of the end of the line to be a cap.
+        //Paints.cap specifies how to the beginning and ending of stroked lines and path look.
+        strokeCap = Paint.Cap.ROUND // default: BUTT
+        //stroke width specifies the width of the stroke in pixels.
+        strokeWidth = STROKE_WIDTH // default: Hairline-width (really thin)
+    }
 
     //Todo 1.4 Provide onSizeChanged. Fill bitmap and create canvas from it
     /**
