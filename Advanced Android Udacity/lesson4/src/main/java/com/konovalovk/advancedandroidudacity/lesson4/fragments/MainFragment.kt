a7bcfb0271cdb94409ba378d56d3202708ca67fc
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-09 13:45:49 +0300

    Exercise 7. Canvas View in fragment - done

--- Advanced Android Udacity/lesson4/src/main/java/com/konovalovk/advancedandroidudacity/lesson4/fragments/MainFragment.kt
+++ Advanced Android Udacity/lesson4/src/main/java/com/konovalovk/advancedandroidudacity/lesson4/fragments/MainFragment.kt
@@ -1,10 +1,29 @@
 package com.konovalovk.advancedandroidudacity.lesson4.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.konovalovk.advancedandroidudacity.lesson4.MainViewModel
+import com.konovalovk.advancedandroidudacity.lesson4.MyCanvasView
 import com.konovalovk.advancedandroidudacity.lesson4.R
 
-class MainFragment : Fragment(R.layout.main_fragment) {
+class MainFragment : Fragment() {
     private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val myCanvasView = MyCanvasView(requireContext())
+        myCanvasView.run {
+            systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+            contentDescription = getString(R.string.canvasContentDescription)
+        }
+        return myCanvasView//super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
