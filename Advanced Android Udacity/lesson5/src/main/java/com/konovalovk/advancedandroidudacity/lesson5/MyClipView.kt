26c3f724c38628eabe86d0bccb6bcfeff7f2c270
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-09 16:29:29 +0300

    Exercise 6. Paint & Path - done

--- Advanced Android Udacity/lesson5/src/main/java/com/konovalovk/advancedandroidudacity/lesson5/MyClipView.kt
+++ Advanced Android Udacity/lesson5/src/main/java/com/konovalovk/advancedandroidudacity/lesson5/MyClipView.kt
@@ -1,12 +1,24 @@
 package com.konovalovk.advancedandroidudacity.lesson5
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
+//Todo: 1.1 Create Custom View
 class ClippedView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+    //Todo: 1.2 Add Paint
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    private val path = Path()
 }
