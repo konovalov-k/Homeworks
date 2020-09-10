e36edc944d36109ccee9917438c7eb4edf1f39fc
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-10 10:44:58 +0300

    Exercise 13. drawClipped&back - done

--- Advanced Android Udacity/lesson5/src/main/java/com/konovalovk/advancedandroidudacity/lesson5/MyClipView.kt
+++ Advanced Android Udacity/lesson5/src/main/java/com/konovalovk/advancedandroidudacity/lesson5/MyClipView.kt
@@ -2,6 +2,7 @@ package com.konovalovk.advancedandroidudacity.lesson5
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -63,8 +64,46 @@ class ClippedView @JvmOverloads constructor(
         // drawQuickRejectExample(canvas)
     }
 
+    //todo: 1.7 Implement drawClippedRectangle&drawBackAndUnclippedRectangle
     private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
+        canvas.drawColor(Color.GRAY)
+        canvas.save()//save the canvas
+        canvas.translate(columnOne,rowOne) //translate\move to the first row and column position,
+        drawClippedRectangle(canvas)
+        canvas.restore()//restore the canvas to its previous state.
     }
+
+    private fun drawClippedRectangle(canvas: Canvas) {
+        canvas.clipRect(//reduces the region of the screen that future draw operations can write to.
+            clipRectLeft,clipRectTop,
+            clipRectRight,clipRectBottom
+        ) //set the boundaries of the clipping rectangle for the whole shape. Apply a clipping rectangle that constrains to drawing only the square.
+        canvas.drawColor(Color.WHITE)
+        paint.color = Color.RED
+        canvas.drawLine(
+            clipRectLeft,clipRectTop,
+            clipRectRight,clipRectBottom,paint
+        )
+
+        paint.color = Color.GREEN
+        canvas.drawCircle(
+            circleRadius,clipRectBottom - circleRadius,
+            circleRadius,paint
+        )
+
+        paint.run {
+            color = Color.BLUE
+            // Align the RIGHT side of the text with the origin.
+            textSize = textSize
+            textAlign = Paint.Align.RIGHT //specifies which side of the text to align to the origin, not which side of the origin the text goes,
+        }
+
+        canvas.drawText(
+            context.getString(R.string.clipping),
+            clipRectRight,textOffset,paint
+        )
+    }
+
     private fun drawDifferenceClippingExample(canvas: Canvas) {
     }
     private fun drawCircularClippingExample(canvas: Canvas) {
