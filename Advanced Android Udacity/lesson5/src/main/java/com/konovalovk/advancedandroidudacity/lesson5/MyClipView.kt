819b845aeb2e347b943601800f5d8b60deaf305c
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-10 11:13:26 +0300

    Exercise 16. drawIntersectionClippingExample - done

--- Advanced Android Udacity/lesson5/src/main/java/com/konovalovk/advancedandroidudacity/lesson5/MyClipView.kt
+++ Advanced Android Udacity/lesson5/src/main/java/com/konovalovk/advancedandroidudacity/lesson5/MyClipView.kt
@@ -167,7 +167,36 @@ class ClippedView @JvmOverloads constructor(
         canvas.restore()
     }
 
+    //Todo: 1.10 Implement drawIntersectionClippingExample
     private fun drawIntersectionClippingExample(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(columnTwo,rowTwo)
+        canvas.clipRect(
+            clipRectLeft,
+            clipRectTop,
+            clipRectRight - smallRectOffset,
+            clipRectBottom - smallRectOffset
+        )
+        // The method clipRect(float, float, float, float, Region.Op
+        // .INTERSECT) was deprecated in API level 26. The recommended
+        // alternative method is clipRect(float, float, float, float), which
+        // is currently available in API level 26 and higher.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            canvas.clipRect(
+                clipRectLeft + smallRectOffset,
+                clipRectTop + smallRectOffset,
+                clipRectRight,clipRectBottom,
+                Region.Op.INTERSECT
+            )
+        } else {
+            canvas.clipRect(
+                clipRectLeft + smallRectOffset,
+                clipRectTop + smallRectOffset,
+                clipRectRight,clipRectBottom
+            )
+        }
+        drawClippedRectangle(canvas)
+        canvas.restore()
     }
     private fun drawCombinedClippingExample(canvas: Canvas) {
     }
