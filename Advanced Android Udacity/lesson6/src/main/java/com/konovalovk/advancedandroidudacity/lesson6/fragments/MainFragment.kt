38d7f513b029f0edb37f83dc0e07954352395d06
konovalovkirill <konovalov.k.a@yandex.ru>
2020-09-10 12:47:13 +0300

    Exercise 4. Translater + refactor - done

--- Advanced Android Udacity/lesson6/src/main/java/com/konovalovk/advancedandroidudacity/lesson6/fragments/MainFragment.kt
+++ Advanced Android Udacity/lesson6/src/main/java/com/konovalovk/advancedandroidudacity/lesson6/fragments/MainFragment.kt
@@ -47,20 +47,26 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     //Todo: 1.1 Rotate view with provided duration and UI block while it animating
     private fun rotater() {
+        //View.ROTATION
+        //Todo: 1.3 Refactor rotater
         val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
-        animator.duration = 1000
-        animator.addListener(object : AnimatorListenerAdapter() {
-            override fun onAnimationStart(animation: Animator?) {
-                rotateButton.isEnabled = false
-            }
-            override fun onAnimationEnd(animation: Animator?) {
-                rotateButton.isEnabled = true
-            }
-        })
-        animator.start()
+        animator.apply {
+            duration = 1000
+            disableViewDuringAnimation(rotateButton, this)
+        }.start()
+
     }
 
+    //Todo: 1.2 Implement translater
     private fun translater() {
+        //View.TRANSLATION_X
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        //Todo: 1.5 Refactor translater
+        animator.apply {
+            repeatCount = 1 // how many times to repeat (or just tell it to run infinitely).
+            repeatMode = ObjectAnimator.REVERSE
+            disableViewDuringAnimation(translateButton, this)
+        }.start()
     }
 
     private fun scaler() {
@@ -73,5 +79,18 @@ class MainFragment : Fragment(R.layout.main_fragment) {
     }
 
     private fun shower() {
+    }
+
+    //Todo: 1.4 Add Extension func to block UI during Animation
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View, animator: ObjectAnimator) {
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
     }
 }
